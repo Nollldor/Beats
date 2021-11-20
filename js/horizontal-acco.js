7ac51297095fa224a7d1afd5afaf227022cd4ac5
Nollldor <90249246+Nollldor@users.noreply.github.com>
2021-11-20 17:01:24 +0500

    горизонтал аккордеон (мобил)

--- js/horizontal-acco.js
+++ js/horizontal-acco.js
@@ -4,15 +4,18 @@ const mesureWidth = item => {
     const container = item.closest(".horizontal-menu");
     const titlesBlocks = container.find(".horizontal-menu__title");
     const titlesWidth = titlesBlocks.width() * titlesBlocks.length;
-    const isMobile = window.matchMedia("(max-width: 768px)").matches;
+    const isTablet = window.matchMedia("(max-width: 768px)").matches;
+    const isMobile = window.matchMedia("(max-width: 480px)").matches;
 
     const textContainer = item.find(".horizontal-menu__container");
     const paddingLeft = parseInt(textContainer.css("padding-left"));
     const paddingRight = parseInt(textContainer.css("padding-right"));
 
-    if (isMobile) {
+    if (isMobile){
+        reqItemWidth = screenWidth - titlesBlocks.width();
+    }else if (isTablet) {
         reqItemWidth = screenWidth - titlesWidth;
-    } else {
+    }else {
         reqItemWidth = 500;
     }
 
@@ -26,8 +29,15 @@ const mesureWidth = item => {
 const closeEveryItemInContainer = (container) => {
     const items = container.find(".horizontal-menu__item");
     const content = container.find(".horizontal-menu__content");
+    const isMobile = window.matchMedia("(max-width: 480px)").matches;
 
     items.removeClass("active");
+    if(isMobile){
+        items.removeClass("active");
+        items.removeClass("not-active");
+    }else {
+        items.removeClass("active");
+    }
     content.width(0);
 }
 
@@ -35,8 +45,13 @@ const openHorisontalItem = (item) => {
     const hiddenContent = item.find(".horizontal-menu__content");
     const reqWidth = mesureWidth(item);
     const textBlock = item.find(".horizontal-menu__container");
+    const isMobile = window.matchMedia("(max-width: 480px)").matches;
 
-    item.addClass("active");
+    if(isMobile){
+        item.addClass("active").siblings().addClass("not-active");
+    }else {
+        item.addClass("active");
+    }
     hiddenContent.width(reqWidth.container);
     textBlock.width(reqWidth.textContainer);
 }
@@ -47,12 +62,15 @@ $(".horizontal-menu__title").on("click", e => {
     const $this = $(e.currentTarget);
     const item = $this.closest(".horizontal-menu__item");
     const itemOpened = item.hasClass("active");
-    const container = $this.closest(".horizontal-menu")
+    const container = $this.closest(".horizontal-menu");
+    
 
     if (itemOpened) {
         closeEveryItemInContainer(container);
+
     } else {
         closeEveryItemInContainer(container);
+
         openHorisontalItem(item);
     }
     
