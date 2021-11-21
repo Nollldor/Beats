ade387fe2d2e88ba06a437dcc48a45bac5359db9
Nollldor <90249246+Nollldor@users.noreply.github.com>
2021-11-21 23:08:01 +0500

    функционал для гамбургер меню

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,13 +1,22 @@
 var modalClose = document.querySelector(".modal__close");
-        var sectionModal = document.querySelector(".section__modal");
-        var hamburger = document.querySelector(".hamburger");
+var sectionModal = document.querySelector(".section__modal");
+var hamburger = document.querySelector(".hamburger");
 
-        modalClose.addEventListener("mousedown", function(){
-            sectionModal.style.display = "none";
-            document.body.style.overflow = "";
-        });
+
+
+
+
+$(".modal-menu__link").on("click", e => {
+    sectionModal.style.display = "none";
+    document.body.style.overflow = "";
+});
+
+modalClose.addEventListener("mousedown", function(){
+    sectionModal.style.display = "none";
+    document.body.style.overflow = "";
+});
         
-        hamburger.addEventListener("mousedown", function(){
-            sectionModal.style.display = "flex";
-            document.body.style.overflow = "hidden";
-        });
+hamburger.addEventListener("mousedown", function(){
+    sectionModal.style.display = "flex";
+    document.body.style.overflow = "hidden";
+    });
